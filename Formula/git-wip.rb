a46b05c782bc12b5762443059797d8bd391e5ee6
Ubuntu <ubuntu@vps-04d2743d.vps.ovh.net>
2026-02-23 08:38:15 +0000

    Add git-wip formula

---
+++ Formula/git-wip.rb
@@ -0,0 +1,27 @@
+class GitWip < Formula
+  desc "List recent git branches across workspaces, detect dirty/unpushed state, batch-commit to WIP branches"
+  homepage "https://github.com/spqw/git-wip"
+  url "https://github.com/spqw/git-wip.git", branch: "main"
+  version "0.1.0"
+  license "MIT"
+
+  depends_on "node"
+  depends_on "pnpm"
+
+  def install
+    system "pnpm", "install", "--frozen-lockfile"
+    libexec.install Dir["*"]
+    libexec.install ".gitignore"
+
+    # Create wrapper that uses the bundled tsx
+    (bin/"git-wip").write <<~BASH
+      #!/usr/bin/env bash
+      exec "#{libexec}/node_modules/.bin/tsx" "#{libexec}/src/cli.ts" "$@"
+    BASH
+    chmod 0755, bin/"git-wip"
+  end
+
+  test do
+    assert_match "0.1.0", shell_output("#{bin}/git-wip --version")
+  end
+end
