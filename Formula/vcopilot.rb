2ed71ac219dbb73ae0d275e53d5f73bc02b9183a
spqw <101190846+spqw@users.noreply.github.com>
2026-02-24 20:18:30 +0000

    vcopilot 1.0.10

--- Formula/vcopilot.rb
+++ Formula/vcopilot.rb
@@ -1,8 +1,8 @@
 class Vcopilot < Formula
   desc "GitHub Copilot CLI - pipe-friendly LLM interface"
   homepage "https://github.com/spqw/lib-copilot"
-  url "https://github.com/spqw/lib-copilot/releases/download/v1.0.9/vcopilot-1.0.9.tgz"
-  sha256 "bc453986edde3a52dc79d3baa6845ab026398e19016af1f4b415fa818c69e1a1"
+  url "https://github.com/spqw/lib-copilot/releases/download/v1.0.10/vcopilot-1.0.10.tgz"
+  sha256 "640105c310479836a32a086d351473bdbc2e2c06a0e9ba8ce649f63d63286ef6"
   license "MIT"
 
   depends_on "node"
